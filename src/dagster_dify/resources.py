4cf751bc4893f0fa6871e1d061e3f26c43e78284
YiZixuan <sqkkyzx@qq.com>
2025-10-19 10:36:16 +0800

    feat(dagster-dify): 支持文件上传和工作流事件日志记录
    - 新增 FileInput 模型以支持多种文件输入类型
    - 实现 upload_file 方法用于上传本地或字节流文件
    - 增强 dify_workflow_stream 方法中的事件日志记录功能
    - 添加 AppInfo 和 WebAppInfo 模型用于解析应用信息
    - 简化 pydantic 和 dagster 的依赖声明
    - 优化 HTTP 请求处理逻辑及超时设置

--- src/dagster_dify/resources.py
+++ src/dagster_dify/resources.py
@@ -1,258 +1,197 @@
+import io
 import json
-from typing import List, Dict
+import logging
+import os
+from typing import List, Dict, Literal, Union, Optional
 
 import httpx
 from dagster import ConfigurableResource, OpExecutionContext, AssetExecutionContext, ExpectationResult
 from pydantic import Field, BaseModel
 
 
+class FileInput(BaseModel):
+    type: Literal["document", "image", "audio", "video", "custom"]
+    transfer_method: Literal["remote_url", "local_file"]
+    url: Optional[str] = None
+    upload_file_id: Optional[str] = None
+
+FileInputs = Union[FileInput, List[FileInput], Dict[str, str], List[Dict[str, str]]]
+
+
 class DifyResource(ConfigurableResource):
     """
 [Dify API](https://docs.dify.ai/zh-hans/guides/application-publishing/developing-with-apis)
-
-### 配置项:
-
-- **AppID** (str):
-    应用应用唯一标识 AppID，作为缓存标识符使用。不传入则不缓存鉴权。
-- **AgentID** (int, optional):
-    原企业内部应用 AgentId ，部分 API 会使用到。默认值为 None
-- **AppName** (str, optional):
-    应用名。
-- **ClientId** (str):
-    应用的 Client ID ，原 AppKey 和 SuiteKey
-- **ClientSecret** (str):
-    应用的 Client Secret ，原 AppSecret 和 SuiteSecret
-
-### 用例
-
-##### 1. 使用单一的企业内部应用资源。
-
-```python
-from dagster_dingtalk import DingTalkAppResource, DingTalkAppClient
-from dagster import op, In, OpExecutionContext, job, Definitions, EnvVar
-
-@op(required_resource_keys={"dingtalk"}, ins={"user_id": In(str)})
-def op_user_info(context:OpExecutionContext, user_id:str):
-    dingtalk:DingTalkAppClient = context.resources.dingtalk
-    result = dingtalk.通讯录管理.用户管理.查询用户详情(user_id).get('result')
-    context.log.info(result)
-
-@job
-def job_user_info():
-    op_user_info()
-
-defs = Definitions(
-    jobs=[job_user_info],
-    resources={"dingtalk": DingTalkAppResource(
-        AppID = "<the-app-id>",
-        ClientId = "<the-client-id>",
-        ClientSecret = EnvVar("<the-client-secret-env-name>"),
-    )})
-```
-
-##### 2. 启动时动态构建企业内部应用资源, 可参考 [Dagster文档 | 在启动时配置资源](https://docs.dagster.io/concepts/resources#configuring-resources-at-launch-time)
-
-```python
-from dagster_dingtalk import DingTalkAppResource, DingTalkAppClient
-from dagster import op, In, OpExecutionContext, job, Definitions, schedule, RunRequest, RunConfig, EnvVar
-
-@op(required_resource_keys={"dingtalk"}, ins={"user_id": In(str)})
-def op_user_info(context:OpExecutionContext, user_id:str):
-    dingtalk:DingTalkAppClient = context.resources.dingtalk
-    result = dingtalk.通讯录管理.用户管理.查询用户详情(user_id).get('result')
-    context.log.info(result)
-
-@job
-def job_user_info():
-    op_user_info()
-
-dingtalk_apps = {
-    "App1" : DingTalkAppResource(
-        AppID = "<app-1-app-id>",
-        ClientId = "<app-1-client-id>",
-        ClientSecret = EnvVar("<app-1-client-secret-env-name>"),
-    ),
-    "App2" : DingTalkAppResource(
-        AppID = "<app-2-app-id>",
-        ClientId = "<app-2-client-id>",
-        ClientSecret = EnvVar("<app-2-client-secret-env-name>"),
-    )
-}
-
-defs = Definitions(jobs=[job_user_info], resources={"dingtalk": DingTalkAppResource.configure_at_launch()})
-
-@schedule(cron_schedule="20 9 * * *", job=job_user_info)
-def schedule_user_info():
-    return RunRequest(run_config=RunConfig(
-        ops={"op_user_info": {"inputs": {"user_id": "<the-user-id>"}}},
-        resources={"dingtalk": dingtalk_apps["App1"]},
-    ))
-```
-
-### 注意:
-
-应该永远避免直接将密钥字符串直接配置给资源，这会导致在 dagster 前端用户界面暴露密钥。你可以在代码中注册临时的环境变量，或从系统中引入环境变量。
     """
 
     BaseURL: str = Field(description="Example: https://<your-domain>/v1")
 
     def dify_workflow_stream(
             self,
-            inputs: Dict[str, str|Dict[str, str]|List],
             token: str,
+            inputs: Dict[str, str|FileInputs],
             user: str = "dagster",
             trace_id: str = None,
-            context: OpExecutionContext | AssetExecutionContext = None
+            context: OpExecutionContext | AssetExecutionContext = None,
+            timeout: int = 30000,
     ) -> Dict:
         llm_workflow_output: Dict|None = None
+
+        app_info = self.app_info(token)
+
         headers = {'Authorization': f'Bearer {token}',}
         if trace_id:
             headers['X-Trace-Id'] = trace_id
 
-        with httpx.Client() as client:
-            with client.stream(
-                    method="POST",
-                    url=f"{self.BaseURL}/workflows/run",
-                    headers=headers,
-                    json={"inputs": inputs, "response_mode": "streaming", "user": user},
-                    timeout=30000,
-            ) as response:
-                for line in response.iter_lines():
-                    if line.startswith("data:"):
-                        event_data = json.loads(line[len("data:"):].strip())
-                        event = event_data.get('event')
+        with httpx.stream(
+            method="POST",
+            url=f"{self.BaseURL}/workflows/run",
+            headers=headers,
+            json={"inputs": inputs, "response_mode": "streaming", "user": user},
+            follow_redirects=True,
+            timeout=timeout,
+        ) as response:
+            node_cache = {}
+            for line in response.iter_lines():
+                if not line.startswith("data:"):
+                    continue
 
-                        task_id = event_data.get('task_id')
-                        workflow_run_id = event_data.get('workflow_run_id')
-                        data = event_data.get('data', {})
-                        status = data.get('status')
-                        error = data.get('error')
+                event_data = json.loads(line[len("data:"):].strip())
+                event = event_data.get('event')
 
-                        node_index = data.get('index')
-                        node_title = data.get('title')
+                task_id = event_data.get('task_id')
+                workflow_run_id = event_data.get('workflow_run_id')
+                data = event_data.get('data', {})
+                status = data.get('status')
+                error = data.get('error')
 
-                        log_label = f"DIFY_{event}".upper()
-                        log_metadata = {"task_id": task_id, "workflow_run_id": workflow_run_id}
+                node_index = data.get('index')
 
-                        match event:
-                            case "workflow_started":
-                                log_success = True
-                                log_description = F"{log_label}: Task Started."
-                            case "node_started":
-                                log_metadata: dict = log_metadata | {
-                                    "node_id": data.get('node_id'),
-                                    "node_type": data.get('node_type'),
-                                    "node_title": node_title,
-                                    "node_index": node_index,
-                                    "predecessor_node_id": data.get('predecessor_node_id'),
-                                    "inputs": data.get('inputs'),
-                                }
-                                log_success = True
-                                log_description = F"{log_label}: Node{node_index} started."
-                            case "node_finished":
-                                log_metadata: dict = log_metadata | {
-                                    "node_id": data.get('node_id'),
-                                    "node_index": node_index,
-                                    "elapsed_time": data.get('elapsed_time'),
-                                    "predecessor_node_id": data.get('predecessor_node_id'),
-                                    "outputs": data.get('outputs'),
-                                    "error": error,
-                                } | (data.get('execution_metadata', {}) or {})
-                                log_description = F"{log_label}: Task {task_id} node{node_index} {status}."
-                                log_success = True if status in ["succeeded", "running"] else False
-                            case "workflow_finished":
-                                log_metadata = log_metadata | {
-                                    "outputs": data.get('outputs'),
-                                    "error": data.get('error'),
-                                    "elapsed_time": data.get('elapsed_time'),
-                                    "total_tokens": data.get('total_tokens'),
-                                    "total_steps": data.get('total_steps'),
-                                }
-                                log_description = F"{log_label}: Task {status}."
-                                log_success = True if status in ["succeeded", "running"] else False
+                log_label = f"DIFY_{event}".upper()
+                log_metadata = {"task_id": task_id, "workflow_run_id": workflow_run_id}
 
-                                llm_workflow_output = data.get("outputs", {})
+                if event == "workflow_started":
+                    context.log_event(ExpectationResult(
+                        success=True,
+                        label=log_label,
+                        description=F"{log_label}: Task Started.",
+                        metadata=log_metadata | app_info.model_dump(),
+                    ))
+                elif event == "node_started":
+                    node_cache[str(node_index)] = data.get('title')
+                    context.log_event(ExpectationResult(
+                        success=True,
+                        label=F"{log_label}: Node{node_index} [{data.get('title')}] started.",
+                        description=F"{log_label}: Task Started.",
+                        metadata=log_metadata | {
+                            "node_title": data.get('title'),
+                            "node_id": data.get('node_id'),
+                            "node_type": data.get('node_type'),
+                            "node_index": node_index,
+                            "predecessor_node_id": data.get('predecessor_node_id'),
+                            "inputs": data.get('inputs'),
+                        },
+                    ))
+                elif event == "node_finished":
+                    context.log_event(ExpectationResult(
+                        success=True if status in ["succeeded", "running"] else False,
+                        label=log_label,
+                        description=F"{log_label}: Task {task_id} node{node_index} {status}.",
+                        metadata=log_metadata | {
+                            "node_title": node_cache.get(str(node_index)),
+                            "error": error,
+                            "elapsed_time": data.get('elapsed_time'),
+                        } | (data.get('execution_metadata', {}) or {}) | {
+                            "node_id": data.get('node_id'),
+                            "node_index": node_index,
+                            "predecessor_node_id": data.get('predecessor_node_id'),
+                            "outputs": data.get('outputs'),
+                        },
+                    ))
+                elif event == "workflow_finished":
+                    context.log_event(ExpectationResult(
+                        success=True if status in ["succeeded", "running"] else False,
+                        label=log_label,
+                        description=F"{log_label}: Task {status}.",
+                        metadata=log_metadata | {
+                            "error": data.get('error'),
+                            "elapsed_time": data.get('elapsed_time'),
+                            "total_tokens": data.get('total_tokens'),
+                            "total_steps": data.get('total_steps'),
+                            "outputs": data.get('outputs'),
+                        },
+                    ))
 
-                        context.log_event(ExpectationResult(
-                            success=log_success,
-                            label=log_label,
-                            description=log_description,
-                            metadata=log_metadata,
-                        ))
+                    llm_workflow_output = data.get("outputs", {})
+                else:
+                    continue
 
         return llm_workflow_output
 
 
-class StreamEventWorkflowStarted(BaseModel):
-    class Data(BaseModel):
-        id: str
-        workflow_id: str
-        created_at: int
-    event: str
-    task_id: str
-    workflow_run_id: str
-    data: Data
+    def upload_file(
+            self,
+            token: str,
+            file_name: Optional[str],
+            file_path: Optional[str] = None,
+            file_bytes: Optional[bytes] = None,
+            user: str = "dagster"
+    ):
+        if not file_name:
+            file_name = os.path.basename(file_path)
 
-class StreamEventNodeStarted(BaseModel):
-    class Data(BaseModel):
-        id: str
-        node_id: str
-        node_type: str
-        title: str
-        index: int
-        predecessor_node_id: str
-        inputs: dict
-        created_at: int
-    event: str
-    task_id: str
-    workflow_run_id: str
-    data: Data
+        if file_bytes is not None:
+            with io.BytesIO(file_bytes) as file:
+                res = httpx.post(
+                    f"{self.BaseURL}/files/upload",
+                    files={"file": (file_name, file)},
+                    json={"user": user},
+                    headers={"Authorization": f"Bearer {token}"},
+                ).json()
+                return res
+        elif file_path is not None:
+            with open(file_path, "rb") as file:
+                res = httpx.post(
+                    f"{self.BaseURL}/files/upload",
+                    files={"file": (file_name, file)},
+                    json={"user": user},
+                    headers={"Authorization": f"Bearer {token}"},
+                ).json()
+                return res
+        else:
+            raise "At least one of file_path or file_bytes should be provided."
 
-class StreamEventTextChunk(BaseModel):
-    class Data(BaseModel):
-        text: str
-        from_variable_selector: str
-    event: str
-    task_id: str
-    workflow_run_id: str
-    data: Data
+    def app_info(self, token: str):
+        res = httpx.get(
+            f"{self.BaseURL}/info",
+            headers={"Authorization": f"Bearer {token}"},
+        ).json()
+        return AppInfo(**res)
 
-class StreamEventNodeFinished(BaseModel):
-    class Execution(BaseModel):
-        total_tokens: int
-        total_price: float
-        currency: str
-    class Data(BaseModel):
-        id: str
-        node_id: str
-        index: int
-        predecessor_node_id: str
-        inputs: dict
-        process_data: dict
-        outputs: dict
-        status: str
-        error: str
-        elapsed_time: float
-        execution_metadata: float
-        created_at: int
-    event: str
-    task_id: str
-    workflow_run_id: str
-    data: Data
+    def webapp_info(self, token: str):
+        res = httpx.get(
+            f"{self.BaseURL}/site",
+            headers={"Authorization": f"Bearer {token}"},
+        ).json()
+        return WebAppInfo(**res)
 
-class StreamEventWorkflowFinished(BaseModel):
-    class Data(BaseModel):
-        id: str
-        workflow_id: str
-        status: str
-        outputs: dict
-        error: str
-        elapsed_time: float
-        total_tokens: int
-        total_steps: int
-        created_at: int
-        finished_at: int
-    event: str
-    task_id: str
-    workflow_run_id: str
-    data: Data
+
+class AppInfo(BaseModel):
+    name: str
+    description: str
+    tags: List[str]
+    mode: str
+    author_name: str
+
+
+class WebAppInfo(BaseModel):
+    title: str
+    icon_type: str
+    icon: str
+    icon_background: str
+    icon_url: str
+    description: str
+    copyright: str
+    privacy_policy: str
+    custom_disclaimer: str
+    default_language: str
+    show_workflow_steps: str
